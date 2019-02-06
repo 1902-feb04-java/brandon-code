d26fb4363340459f6d5336290d745fda86ce9ff6
Brandon <tubocass@gmail.com>
2019-02-06 11:53:41 -0600

    removed eslint

--- Javascript-basics/main.js
+++ Javascript-basics/main.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-console */
 /*
     JAVASCRIPT
 */
+
+console.log("Hello JS!");
+
+let x = {hour:3};
+console.log("Value of x: "+ x);
+console.log("type of x:" + typeof(x));
+// debugger;
+x.hour = 5;
+console.log("Value of x: "+ x);
+console.log("type of x:" + typeof(x));
+
+const obj= {
+    age: 18
+}
