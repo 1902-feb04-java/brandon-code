5888f455e840391b1e71c11a5976bd2bc95f8eb9
Brandon <tubocass@gmail.com>
2019-02-07 13:59:21 -0600

    login page with js

--- Javascript-basics/main.js
+++ Javascript-basics/main.js
@@ -16,3 +16,13 @@ console.log("type of x:" + typeof(x));
 const obj= {
     age: 18
 }
+
+function newCounter() {
+    let inc = 0;
+    return function(){inc++; return inc;};
+}
+let counter = newCounter();
+console.log(counter());
+console.log(counter());
+
+console.log(counter());
